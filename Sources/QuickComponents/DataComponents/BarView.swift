71e23b305d7c554891cc363d1530ff76c9647f16
Lucas Desouza <lucasfds2777@gmail.com>
2020-07-12 00:05:41 -0500

    fix(data-components): fixes overflow bug with bar view component (#10)

--- Sources/QuickComponents/DataComponents/BarView.swift
+++ Sources/QuickComponents/DataComponents/BarView.swift
@@ -14,6 +14,7 @@ public struct BarView: View {
     
     var value: CGFloat
     var max: CGFloat
+    var overflowWarning = false
     var showLabel: Bool = false
     var color: Color = Color(#colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1))
     
@@ -22,7 +23,12 @@ public struct BarView: View {
     ///   - value: FIlled in volume of the bar
     ///   - max: Max volume of the bar
     public init(value: CGFloat, max: CGFloat){
-        self.value = value
+        if value > max {
+            self.value = max
+            self.overflowWarning = true
+        } else {
+            self.value = value
+        }
         self.max = max
     }
     
@@ -50,8 +56,18 @@ public struct BarView: View {
             ZStack (alignment: .leading) {
                 Capsule().frame(width: geometry.size.width, height: 40).foregroundColor(Color.offWhite).shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
                 Capsule().frame(width: self.calcWidth(value: self.value, max: self.max, barWidth: geometry.size.width), height: 30).foregroundColor(self.color).padding(5).shadow(color: Color.black.opacity(0.2), radius: 10, x: 10, y: 10)
-                if self.showLabel {
-                    Text(String(format: "%.2f", self.value)).padding()
+                HStack{
+                    if self.showLabel {
+                        Text(String(format: "%.2f", self.value)).padding()
+                    }
+                    Spacer()
+                    if self.overflowWarning {
+                        ZStack{
+                            Circle().frame(width: 25, height:25).foregroundColor(.red)
+                            Text("!")
+                        }.Paragraph(align: .trailing, size: .LG).foregroundColor(.white)
+                        
+                    }
                 }
             }
         }
@@ -69,6 +85,7 @@ struct BarView_Previews_neumorphism: PreviewProvider {
             Color.offWhite.edgesIgnoringSafeArea(.all)
             VStack {
                 BarView(value: 22, max: 30)
+                BarView(value: 35, max: 30)
                 BarView(value: 30, max: 30)
                 BarView(value: 22, max: 30, showLabel: nil, color: .green)
                 BarView(value: 22, max: 30, showLabel: true, color: nil)
@@ -83,6 +100,7 @@ struct BarView_Previews: PreviewProvider {
     static var previews: some View {
         VStack {
             BarView(value: 22, max: 30)
+            BarView(value: 35, max: 30)
             BarView(value: 30, max: 30)
             BarView(value: 22, max: 30, showLabel: nil, color: .green)
             BarView(value: 22, max: 30, showLabel: true, color: nil)
